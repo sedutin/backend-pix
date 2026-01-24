98c7b397f1ece5d5bd71e0ff3b45daacfb33840e
sedu <edhynhoalves.br@gmail.com>
2026-01-24 00:13:19 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const ACCESS_TOKEN = process.env.MP_TOKEN;
+
+app.get("/", (req, res) => {
+  res.send("API Pix Sedutin online 🚀");
+});
+
+app.post("/pix", async (req, res) => {
+  try {
+    const { valor, descricao, email } = req.body;
+
+    const pagamento = await axios.post(
+      "https://api.mercadopago.com/v1/payments",
+      {
+        transaction_amount: Number(valor),
+        description: descricao,
+        payment_method_id: "pix",
+        payer: { email }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${APP_USR-4112858295555247-012013-b81f5580e3abe3c8b90de07373a6cb22-1079395237}`
+        }
+      }
+    );
+
+    res.json(pagamento.data);
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    res.status(500).json({ erro: "Erro ao gerar Pix" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Pix online na porta " + PORT);
+});
